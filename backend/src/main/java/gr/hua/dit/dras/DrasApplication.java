32a8ee88ef50e1beaffb4edff8df0a330bd671da
Athos Exarchou <athosexarhou@gmail.com>
2025-11-17 13:29:43 +0200

    feat: Setup Spring Boot project skeleton

---
+++ backend/src/main/java/gr/hua/dit/dras/DrasApplication.java
@@ -0,0 +1,14 @@
+package gr.hua.dit.dras;
+
+/* imports */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrasApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/gr/hua/dit/dras/DrasApplicationTests.java
@@ -0,0 +1,14 @@
+package gr.hua.dit.dras;
+
+/* imports */
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
