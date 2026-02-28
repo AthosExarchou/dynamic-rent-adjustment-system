3a422410b334d4865dcb2d64a0f8d2c18741fa1c
Athos Exarchou <athosexarhou@gmail.com>
2026-02-28 13:32:37 +0200

    feat: introduce DTOs for owner and tenant creation requests

---
+++ backend/src/main/java/gr/hua/dit/dras/dto/OwnerCreateRequest.java
@@ -0,0 +1,52 @@
+package gr.hua.dit.dras.dto;
+
+/* imports */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class OwnerCreateRequest {
+
+    @NotNull(message = "User ID is required")
+    private Integer userId;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
