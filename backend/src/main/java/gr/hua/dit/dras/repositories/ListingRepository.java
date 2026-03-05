4e967d47899fa9ef0aaa28b4fe8456aed3870a2c
Athos Exarchou <athosexarhou@gmail.com>
2026-03-05 13:25:46 +0200

    feat: extend ListingRepository with filtering support

--- backend/src/main/java/gr/hua/dit/dras/repositories/ListingRepository.java
+++ backend/src/main/java/gr/hua/dit/dras/repositories/ListingRepository.java
@@ -5,16 +5,15 @@ import gr.hua.dit.dras.entities.Listing;
 import gr.hua.dit.dras.entities.Owner;
 import gr.hua.dit.dras.model.enums.ListingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ListingRepository extends JpaRepository<Listing, Integer> {
+public interface ListingRepository extends JpaRepository<Listing, Integer>, JpaSpecificationExecutor<Listing> {
 
     List<Listing> findByOwner(Owner owner);
-    List<Listing> findByPriceBetween(int minPrice, int maxPrice);
-    List<Listing> findByTitleContainingIgnoreCaseAndPriceBetween(String title, int minPrice, int maxPrice);
     Optional<Listing> findBySourceUrl(String sourceUrl);
     List<Listing> findByExternalFalse();
     List<Listing> findByExternalTrue();
